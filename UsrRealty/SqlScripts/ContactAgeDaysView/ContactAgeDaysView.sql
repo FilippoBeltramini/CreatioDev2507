6d5fa8f45bd5a6944c1d745367e8789a0ec92f06
FilippoBeltramini <filippo.beltramini@rvc.ch>
2025-07-10 11:25:45 +0200

    Fixed mistake

--- UsrRealty/SqlScripts/ContactAgeDaysView/ContactAgeDaysView.sql
+++ UsrRealty/SqlScripts/ContactAgeDaysView/ContactAgeDaysView.sql
@@ -1,5 +1,7 @@
+drop view if exists public."UsrVwContactAgeDays";
 create or replace view public."UsrVwContactAgeDays"
 as
 select "Id" as "UsrId", "Name" as "UsrName", "BirthDate" as "UsrBirthDate",
-CURRENT_DATE -"BirthDate" as "UsrAgeDays"
-from public."Contact"
+CURRENT_DATE -"BirthDate" as "UsrAgeDays",
+	"Id" as "UsrContactId"
+from public."Contact";
---
+++ UsrRealty/SqlScripts/deleteBlueLight/deleteBlueLight.sql
@@ -0,0 +1,2 @@
+delete from public."UsrTrafficLight"
+where "Name" LIKE '%Blue%'
